c81dd75fa32bacc16ce187294093755e1132f6ee
KM <matsukana3016@gmail.com>
2021-11-01 22:02:29 +0900

    add new file

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# LINE-Developers
+# LINE Messaging API
+
+# 本プロジェクトの構成について
+
+## Laravel バージョン
+
+````
+% php artisan --version
+ Laravel Framework 8.68.1
+````
+
+## PHP バージョン
+
+````
+7.4.12
+````
+
+## データベース
+
+````
+MySQL使用
+````
+
+
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '98020eef4ba207054639482971b4c1d8e8c71860',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '98020eef4ba207054639482971b4c1d8e8c71860',
+            'dev_requirement' => false,
+        ),
+        'linecorp/line-bot-sdk' => array(
+            'pretty_version' => '7.2.0',
+            'version' => '7.2.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../linecorp/line-bot-sdk',
+            'aliases' => array(),
+            'reference' => 'ff9426df7904cc4e2e09b46189db4a52d8bcb28e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
